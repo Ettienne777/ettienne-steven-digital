b56d90c729f5e9ef5fd1bf23cf177a377743e605
Ettienne777 <ettienne06@icloud.com>
2026-06-25 13:06:53 +0200

    "remove duplicate folder"

--- ettienne steven digital/src/lib/gsap.ts
+++
@@ -1,7 +0,0 @@
-import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-// Register GSAP plugins once, app-wide.
-gsap.registerPlugin(ScrollTrigger)
-
-export { gsap, ScrollTrigger }
